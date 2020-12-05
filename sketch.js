b999911108ec72946987e7e5a8b60ef3e7d2c227
2712hiral <75423267+2712hiral@users.noreply.github.com>
2020-12-05 16:13:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,24 @@
 var monkey , monkey_running;
 var banana ,bananaImage, obstacle, obstacleImage;
 var FoodGroup, obstacleGroup;
-var score;
-var injury;
-var PLAY;
-var END;
+var score=0;
+var injury=0;
+var PLAY=1;
+var END=0;
 var gameState=PLAY;
+var stroke;
+var gameover;
 
    function preload(){ 
-monkey_running =               loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+monkey_running =                         loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
 
-    bananaImage = loadImage("banana.png");
-    obstaceImage = loadImage("obstacle.png");
-
-  }
+ bananaImage = loadImage("banana.png");
+ obstaceImage = loadImage("obstacle.png");
+ }
 
   function setup() {
     createCanvas(600, 400)
-  ground = createSprite(200, 380, 1900, 40);
+    ground = createSprite(200, 380, 1900, 40);
     monkey = createSprite(55, 330, 20, 20)
 
     monkey.addAnimation("mo", monkey_running);
@@ -30,15 +31,18 @@ monkey_running =               loadAnimation("sprite_0.png","sprite_1.png","spri
     fruitGroup.setLifetimeEach=(-1)
   }
 
-  function draw() {
-     monkey.collide(ig)
-   if(background.x<0){
-        background.x= background.width/2
+function draw() {
+    background("white")
+    monkey.collide(ig)
+    if(ground.x<0){
+        ground.x= ground.width/2
     }
+    
     monkey.velocityY = monkey.velocityY + 15
-
+  
+// gamestate to play
     if(gameState===PLAY){
-    if (keyDown("space") && monkey.y >= 150) {
+    if (keyDown("space") && monkey.y >=150) {
       monkey.velocityY = -12
     }
     
@@ -51,7 +55,7 @@ monkey_running =               loadAnimation("sprite_0.png","sprite_1.png","spri
       break;
       case 40: monkey. scale= 0.21;
       break;
-    default: break;
+      default: break;
   }
       
       if(monkey.isTouching(fruitGroup)){
@@ -59,7 +63,7 @@ monkey_running =               loadAnimation("sprite_0.png","sprite_1.png","spri
       fruitGroup.destroyEach();
     }
       
-     banana();
+    banana();
     obstacle();
 
    if(monkey.isTouching(obstacleGroup)){
@@ -69,40 +73,43 @@ monkey_running =               loadAnimation("sprite_0.png","sprite_1.png","spri
      }    
   }  
    if (injury===2){
-        gameState=END
-
+        gameState=END   
+     }
+    
+  // gamestate to end
     if(gameState===END){
       fruitGroup.setVelocityXEach(0)
        fruitGroup.destroyEach();
       obstacleGroup.destroyEach();
        monkey.velocityY=0
+      
       obstacleGroup.setVelocityXEach(0);
       stroke("black");
       textSize(20);
       fill("black");
-      text("Gameover: " + gameover,180,200);
-        score=0
-      }
+      text("Gameover",200,200);
+      score=0      
     }
-    } 
-
-   //Displaying sprites
-    drawSprites();
-
-    stroke("white");
+    
+   stroke("pink");
     textSize(20);
-    fill("white")
+    fill("black")
     text("Score:" + score, 500, 50);
-  
-  function banana() {
+
+//Displaying sprites
+    drawSprites();
+ }
+
+function banana() {
     if (frameCount % 80 == 0) {
       fruit = createSprite(600, 200, 20, 20);
-      fruit.velocityX = -(7+(score/10));
+      fruit.velocityX = -(10+(score/10));
       fruit.y = Math.round(random(120, 360));
       fruit.addImage(bananaImage);
       fruit.scale = 0.2
       fruitGroup.add(fruit);
-      fruit.lifetime = 600;
+      fruit.lifetime = 60;
+      fruit.velocityX=-10;
     }
     monkey.debug = false
     monkey.setCollider("circle",0,0,200)
@@ -112,27 +119,12 @@ monkey_running =               loadAnimation("sprite_0.png","sprite_1.png","spri
   if(frameCount%300==0){
     ob=createSprite(600,360,20,20);
     var rand= Math.round(random(1,6));
-    switch (rand) {
-      case 1:ob.addImage(obstaceImage)
-        break;
-        case 2:ob.addImage(obstaceImage)
-        break
-        case 3:ob.addImage(obstaceImage)
-        break;
-        case 4:ob.addImage(obstaceImage)
-        break;
-        case 5:ob.addImage(obstaceImage)
-        break;
-        case 6:ob.addImage(obstaceImage)
-        break
-        default:break;
-    }
-
     ob.velocityX=-(8+(score/10))
-    ob.lifetime=300;
+    ob.lifetime=60;
     ob.addImage(obstaceImage);
     ob.scale=0.2;
     obstacleGroup.add(ob);
     ob.debug=false
+    ob.velocityX=-10;
   } 
-  }
+  } 
